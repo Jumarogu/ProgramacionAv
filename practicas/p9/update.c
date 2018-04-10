4db41d2309f8f85b840f21bebef7b2a56f7f8dd7
Jumarogu <jmromerog511@gmail.com>
2018-04-09 19:30:36 -0500

    practica 9

---
+++ practicas/p9/update.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct s {
+    char firstName[20];
+    char lastName[20];
+    short ID;
+    char semester[3];
+} Student;
+
+int main(int argc, char **argv) {
+
+    char *search = argv[1];
+    char *replace = argv[2];
+
+    char *dbName = "student.dat";
+    Student myStudent;
+    
+    FILE *data = fopen(dbName, "r+");
+    while(1) {
+        fread(&myStudent, sizeof(Student), 1, data);
+        if(feof(data)){
+            break;
+        }
+        if(!strcmp(myStudent.firstName, search)) {
+            strcpy(myStudent.firstName, replace);
+            fseek(data, -1*sizeof(Student), SEEK_CUR);
+            fwrite(&myStudent, sizeof(Student), 1, data);
+            break;
+        }
+    }
+
+    return 0;
+}
